7f9666f41d77edc49d090e6716185c0fd322979c
Andy Patterson <andnpatterson@gmail.com>
2024-05-02 11:26:05 -0600

    fix: switch to debug level logging

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -3,6 +3,6 @@ logger = logging.getLogger('gh-metrics')
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
+    level=logging.DEBUG,
     datefmt='%Y-%m-%d %H:%M:%S',
 )
